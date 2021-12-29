addd95d653c046ff7faae6d0a5e45d8e3407c62d
Jason <jasonli.0616@outlook.com>
2021-12-28 23:32:24 -0500

    Edit titlebar

--- src/components/Titlebar/Titlebar.jsx
+++ src/components/Titlebar/Titlebar.jsx
@@ -7,8 +7,11 @@ export default function Titlebar() {
             <h1 style={{ cursor: 'pointer' }} onClick={() => {
                 window.location.href = `${new URL(window.location.href).origin}/seconds-countdown`;
             }}>
-            Seconds Countdown
+                Seconds Countdown
             </h1>
+            <p>
+                Made by: <a href="https://jasonli0616.dev" target="_blank" rel="noreferrer" style={{color: 'cyan'}}>Jason</a>
+            </p>
         </div>
     );
 };
